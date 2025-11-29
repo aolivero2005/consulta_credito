33ec99240f8c7d3dc239acd945fa608c143d3e27
Alexei Olivero <aolivero2005@gmail.com>
2025-11-29 09:08:36 -0300

    Add initial configuration files and basic test class
    - Add `application.properties` with H2 database and Swagger comments.
    - Add `data.sql` placeholder for future use.
    - Create `CreditoApplicationTests` with a basic `contextLoads` test.

---
+++ src/test/java/aom/credito/challenge/CreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package aom.credito.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
